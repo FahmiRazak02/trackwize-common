bcbfbbcf003c71961f982adc401bf9e00e1679a2
aiman.fahmi <aiman.fahmi@beans.com.my>
2026-01-18 00:27:44 +0800

    ++ update

--- src/main/java/com/trackwize/common/util/PasswordUtil.java
+++ src/main/java/com/trackwize/common/util/PasswordUtil.java
@@ -4,6 +4,10 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 
 public class PasswordUtil {
 
+    private PasswordUtil() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
     public static String encryptPassword(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt(12));
     }
